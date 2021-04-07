c09f7d50678669ec9b044f2014f096c235134bb7
Erickc4 <35501340+Erickc4@users.noreply.github.com>
2021-04-07 15:38:31 -0300

    Add files via upload

---
+++ EXERCICIO11.py
@@ -0,0 +1,9 @@
+nota1 = int(input('Digite a primeira nota: '))
+nota2 = int(input('Digite a segunda nota: '))
+media = (nota1 + nota2)/2
+if media == 10:
+    print('Aprovado com Distinção.')
+elif media >= 7 and media < 10:
+    print('Aprovado.')
+else:
+    print('Reprovado.')
